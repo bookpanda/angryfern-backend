617c77137980e8ee49db6a2d2551c4c44bf79094
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-15 16:54:58 +0700

    feat: main

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"github.com/bookpanda/angryfern-backend/cfgldr"
+	"github.com/bookpanda/angryfern-backend/database"
+
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	conf, err := cfgldr.LoadConfig()
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Str("service", "file").
+			Msg("Failed to load config")
+	}
+
+	_, err = database.New(&conf.DatabaseConfig)
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Str("service", "file").
+			Msg("Failed to init postgres connection")
+	}
+}
--- database/postgres.go
+++ database/postgres.go
@@ -1,12 +1,12 @@
 package database
 
 import (
-	"bookpanda/angryfern-backend/cfgldr"
+	"github.com/bookpanda/angryfern-backend/cfgldr"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func New(conf *cfgldr.DatabaseConfig, isDebug bool) (db *gorm.DB, err error) {
+func New(conf *cfgldr.DatabaseConfig) (db *gorm.DB, err error) {
 	return gorm.Open(postgres.Open(conf.Url), &gorm.Config{TranslateError: true})
 }
