3775227c41e0d4feb9b8dad49531b3bde44d6f85
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-15 17:23:17 +0700

    feat: makefile

---
+++ Makefile
@@ -0,0 +1,14 @@
+dev:
+	go run cmd/main.go
+
+inspect: 
+	atlas schema inspect \
+	--url "postgres://root:1234@localhost:5432/angryfern_db?search_path=public&sslmode=disable" \
+	--format "{{ sql . }}" > ./migrations/init.sql
+
+FILE :=
+migrate:
+	atlas schema apply \
+	--url "postgres://root:1234@localhost:5432/angryfern_db?&sslmode=disable" \
+	--to "file://./migrations/$(FILE)" \
+	--dev-url "docker://postgres/15"
--- migrations/00-init.sql
+++ migrations/00-init.sql
@@ -1,9 +1,9 @@
 -- Create "scores" table
 CREATE TABLE "scores" (
     "code" VARCHAR(10) NOT NULL,
-    "created_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    "name" VARCHAR(100) NOT NULL,
-    "count" INTEGER NOT NULL DEFAULT 0,
+    -- "created_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    -- "updated_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    "country_name" VARCHAR(100) NOT NULL,
+    "click_count" INTEGER NOT NULL DEFAULT 0,
     PRIMARY KEY ("code")
 )
