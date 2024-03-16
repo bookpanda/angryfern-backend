eb1ae279554eb60d3be53273a57942fc07ed8fff
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-16 16:43:58 +0700

    feat: basic main

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/bookpanda/angryfern-backend/cfgldr"
 	"github.com/bookpanda/angryfern-backend/database"
+	"github.com/bookpanda/angryfern-backend/internal/router"
+	"github.com/bookpanda/angryfern-backend/internal/score"
+	"github.com/bookpanda/angryfern-backend/logger"
 )
 
 func main() {
@@ -13,8 +16,24 @@ func main() {
 		panic(fmt.Sprintf("Failed to load config: %v", err))
 	}
 
-	_, err = database.New(conf)
+	db, err := database.New(conf)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to load database: %v", err))
+	}
+
+	logger := logger.New(conf)
+
+	scoreRepo := score.NewRepository(db)
+	scoreSvc := score.NewService(scoreRepo, logger)
+	scoreHdr := score.NewHandler(scoreSvc, logger)
+
+	corsHandler := cfgldr.MakeCorsConfig(conf)
+	r := router.New(conf, corsHandler)
+
+	r.PUT("/scores/:code", scoreHdr.Increment)
+	r.GET("/scores", scoreHdr.GetAllScore)
+
+	if err := r.Run(fmt.Sprintf(":%v", conf.AppConfig.Port)); err != nil {
+		logger.Fatal("unable to start server")
 	}
 }
