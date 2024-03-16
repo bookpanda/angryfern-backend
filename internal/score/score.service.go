51d8d27985b8f6c1a257f3639753618c31f426d5
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-16 16:18:05 +0700

    feat: svc basic

--- internal/score/score.service.go
+++ internal/score/score.service.go
@@ -1,13 +1,15 @@
 package score
 
 import (
+	"github.com/bookpanda/angryfern-backend/errors"
 	"github.com/bookpanda/angryfern-backend/internal/dto"
+	"github.com/bookpanda/angryfern-backend/internal/model"
 	"go.uber.org/zap"
 )
 
 type Service interface {
-	Increment(req *dto.IncrementScoreRequest) (dto.IncrementScoreResponse, error)
-	GetAllScore() (dto.GetAllScoreResponse, error)
+	Increment(req *dto.IncrementScoreRequest) (*dto.IncrementScoreResponse, *errors.AppError)
+	GetAllScore() (*dto.GetAllScoreResponse, *errors.AppError)
 }
 
 func NewService(repo Repository, logger *zap.Logger) Service {
@@ -21,10 +23,25 @@ type serviceImpl struct {
 	logger *zap.Logger
 }
 
-func (s *serviceImpl) Increment(req *dto.IncrementScoreRequest) (dto.IncrementScoreResponse, error) {
-	return dto.IncrementScoreResponse{}, nil
+func (s *serviceImpl) Increment(req *dto.IncrementScoreRequest) (*dto.IncrementScoreResponse, *errors.AppError) {
+	err := s.repo.Increment(req.Code, req.Amount)
+	if err != nil {
+		s.logger.Error("failed to increment score", zap.Error(err))
+		return nil, errors.InternalError
+	}
+
+	return &dto.IncrementScoreResponse{Success: true}, nil
 }
 
-func (s *serviceImpl) GetAllScore() (dto.GetAllScoreResponse, error) {
-	return dto.GetAllScoreResponse{}, nil
+func (s *serviceImpl) GetAllScore() (*dto.GetAllScoreResponse, *errors.AppError) {
+	var query []model.Score
+	err := s.repo.GetAll(&query)
+	if err != nil {
+		s.logger.Error("failed to get all scores", zap.Error(err))
+		return nil, errors.InternalError
+	}
+
+	results := s.modelToDTOList(&query)
+
+	return &dto.GetAllScoreResponse{Scores: results}, nil
 }
